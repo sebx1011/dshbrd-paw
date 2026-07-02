6f9ab9af5a6e1ec8abea9ec0e6de9a80a3064d7a
Sebastian <seba.n.a@outlook.com>
2026-07-02 17:37:59 -0400

    Add health endpoint using Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
+from app.routes.home import home_bp
+from app.routes.health import health_bp
 
 def create_app():
     app = Flask(__name__)
-    
-
-    @app.route('/')
-    def home():
-        return 'Hello, World, alo!'
-
+    app.register_blueprint(home_bp)
+    app.register_blueprint(health_bp)
     return app
